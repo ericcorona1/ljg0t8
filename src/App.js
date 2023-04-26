2acb2965bd54a4c3334eb3b774187b985b30d418
ericcorona1 <ericcorona3@gmail.com>
2023-04-26 00:31:05 -0700

    created 3x3 board

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import React from 'react'
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <div className='board'>
+      <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+      <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+      <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+    </div>
+  );
 }
