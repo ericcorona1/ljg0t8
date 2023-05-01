a43ff50dad14aea67119576ba0dfeea504245391
ericcorona1 <ericcorona3@gmail.com>
2023-05-01 12:37:39 -0700

    created back to start button
    now rendering move history buttons

--- src/App.js
+++ src/App.js
@@ -54,10 +54,23 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+  const moves = history.map((squares, move) => {
+    let description;
+    move > 0 ? description = "Go to move #" + move : description = "Go to game start";
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
   }
 
   return (
@@ -66,7 +79,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
