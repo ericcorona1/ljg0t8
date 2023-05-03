be977d3d5d8ce8ec953acb23f2288fb7b52776e3
ericcorona1 <ericcorona3@gmail.com>
2023-05-03 14:51:23 -0700

    history on, can jump

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
   const moves = history.map((squares, move) => {
     let description;
     move > 0 ? description = "Go to move #" + move : description = "Go to game start";
@@ -65,12 +66,15 @@ export default function Game() {
   });
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove +  1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   return (
